7ef5a2174d0430ec4520950071ab9a5f77b4120e
fq <fq@jtang.cn>
2015-04-15 11:38:41 +0800

    .

--- XiaoMa/Class/ShopDetailVC.m
+++ XiaoMa/Class/ShopDetailVC.m
@@ -30,11 +30,12 @@
     [super viewDidLoad];
 }
 
+
 //- (void)reloadDatasource
 //{
 //    JTShop *shop = [JTShop new];
 //    shop.title = @"神州洗车";
-//    shop.logoUrl = @"tmp_ad";
+//    shop.logoUrl = @"tmp_1";
 //    shop.allowABC = @YES;
 //    shop.allowTicket = @YES;
 //    shop.rating = @4.0;
@@ -66,14 +67,14 @@
 //    JTShopComment *comment1 = [JTShopComment new];
 //    comment1.userName = @"超能陆战队";
 //    comment1.time = @"2015.01.05";
-//    comment1.avatarUrl = @"tmp_ad";
+//    comment1.avatarUrl = @"tmp_a1";
 //    comment1.rating = @4;
 //    comment1.content = @"第一次过来洗车，洗的很干净。老板服务态度很好";
 //    
 //    JTShopComment *comment2 = [JTShopComment new];
 //    comment2.userName = @"陈大白";
 //    comment2.time = @"2015.01.02";
-//    comment2.avatarUrl = @"tmp_ad1";
+//    comment2.avatarUrl = @"tmp_a2";
 //    comment2.rating = @5;
 //    comment2.content = @"洗的很仔细嘛，就给好评了";
 //    shop.comments = @[comment1, comment2];
@@ -82,8 +83,6 @@
 //    self.shop = shop;
 //    [self.tableView reloadData];
 //}
-
-
 #pragma mark - Action
 - (IBAction)actionMap:(id)sender
 {
@@ -217,6 +216,7 @@
     double shopLng = shop.shopLongitude;
     NSString * disStr = [DistanceCalcHelper getDistanceStrLatA:myLat lngA:myLng latB:shopLat lngB:shopLng];
     distantL.text = disStr;
+
     
     return cell;
 }
