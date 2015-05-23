d4d48e73884060973767a01e5b168a1ea7b0fa58
fq <fq@jtang.cn>
2015-05-23 12:01:27 +0800

    礼包接口封装

---
+++ XiaoMa/Class/GetUserCouponPkg.h
@@ -0,0 +1,16 @@
+//
+//  GetUserCouponPkg.h
+//  XiaoMa
+//
+//  Created by jt on 15-5-23.
+//  Copyright (c) 2015年 jiangjunchen. All rights reserved.
+//
+
+#import "BaseOp.h"
+
+@interface GetUserCouponPkg : BaseOp
+
+/// 礼包数组
+@property (nonatomic,strong)NSArray * rsp_pkgArray;
+
+@end
