f0e05913d0e6bf7658bb1478d227086e6bca2655
fq <fq@jtang.cn>
2015-06-29 19:08:13 +0800

    脚本

--- Script/appstore.sh
+++ Script/appstore.sh
@@ -2,10 +2,9 @@
 
 # appstore－xmdd对应的信息
 #appstore_provisioning_id='a2a738ce-17bb-4752-882d-96c2857f9244'
-appstore_provisioning_id='5cd20223-a14a-4639-bc86-0b0dd85e1adf'
+appstore_provisioning_id='0b097263-ee87-4d3e-a27d-1c4766c784b5'
 appstore_code_sign_id='iPhone Distribution: Hangzhou Huika Technology Co.,Ltd (7A3B9332PS)'
 
-#############################################################í#####################
 #脚本目录
 script_path=$(pwd)
 
@@ -22,78 +21,17 @@ cd $root_path"/xmdd_ios"
 project_path=$(pwd)
 project_pbxproj_path=$project_path"/XiaoMa.xcodeproj/project.pbxproj"
 echo "project_pbxproj_path : "$project_pbxproj_path
-# echo $project_pbxproj_path
 
-echo "**************pull code**************"
-git checkout .
-git checkout dev
-if git pull;then
-	echo "git pull success"
-else 
-	echo "git pull error"
-	exit 1
-fi
-
-
-# echo "**************update Version**************"
-# sh $project_path"/Script/plist_replace.sh" $project_path"/XiaoMa/Misc/Info.plist"
-# bundleVersion=$(/usr/libexec/PlistBuddy -c "print CFBundleVersion" $project_path"/XiaoMa/Misc/Info.plist")
-# echo $bundleVersion
-# # git add . && git commit -a -m "change version" && git push
-# if git add . ; then
-# 	if git commit -a -m "change version";then
-# 		if git push;then
-# 			echo "git push success"
-# 		else
-# 			echo "git push error"
-# 			exit 1
-# 		fi
-# 	else
-# 		echo "git commit error"
-# 		exit 1
-# 	fi
-# else
-# 	echo "git add. error"
-# 	exit 1
-# fi
-# echo "**************pull finish**************"
-
-#删除缓存。以前的终端编译会导致后面的编译失败
-user=$USER
-derivedData="/Users/"$user"/Library/Developer/Xcode/DerivedData"
-cd $derivedData && rm -rf *
-
-
-# build appstore
 # 切换到脚本目录
 echo "**************switch script**************"
 cd $script_path
-echo "**************replace pbxproj file**************"
+echo "**************替换证书**************"
 sh project_replace.sh "$appstore_code_sign_id" "$appstore_provisioning_id" "$project_pbxproj_path"
-echo "**************finish replace pbxproj file**************"
 
-echo "**************begin building1**************"
-echo $project_path
-cd $project_path
-security unlock-keychain -p ${1} ~/Library/Keychains/login.keychain
 
-# 先clean
-xcodebuild -project XiaoMa.xcodeproj clean 
+echo "**************替换bundle id,url Scheme，displayName**************"
+sh $project_path"/Script/change_to_appstore.sh" $project_path"/XiaoMa/Misc/Info.plist"
 
-# build
-xcworkspace_name="XiaoMa.xcworkspace"
-scheme_name="XiaoMa"
-configuration_type="Release"
-build_dir=$root_path"/build/ios-xmdd-"$bundleVersion
-ipa_name="ios-xmdd-"$bundleVersion".ipa"
+echo "**************替换环境**************"
+sed -i  '' "s/XMDDENT=1/XMDDENT=0/" $project_pbxproj_path
 
-aaa="xcodebuild -workspace $xcworkspace_name -scheme $scheme_name -configuration $configuration_type CONFIGURATION_BUILD_DIR=$build_dir ONLY_ACTIVE_ARCH=NO"
-echo $aaa
-xcodebuild -workspace $xcworkspace_name -scheme $scheme_name -configuration $configuration_type CONFIGURATION_BUILD_DIR=$build_dir ONLY_ACTIVE_ARCH=NO
-
-# archieve
-archieve_dir=$root_path"/ipa"
-
-xcrun -sdk iphoneos PackageApplication -v $build_dir"/XiaoMa.app" -o $archieve_dir"/"$ipa_name
-
-echo "**************finish building1**************"
