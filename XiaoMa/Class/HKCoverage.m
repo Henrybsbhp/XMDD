5df22e87dc267e1b3bff458277f55dfab9186db3
fq <fq@jtang.cn>
2015-07-31 16:13:56 +0800

    ci

---
+++ XiaoMa/Class/HKCoverage.m
@@ -0,0 +1,15 @@
+//
+//  HKCoverage.m
+//  XiaoMa
+//
+//  Created by jt on 15/7/31.
+//  Copyright (c) 2015年 jiangjunchen. All rights reserved.
+//
+
+#import "HKCoverage.h"
+
+@implementation HKCoverage
+
+- 
+
+@end
