35634374893b331f71e1ed980c71002590adaf5a
fq <fq@jtang.cn>
2015-07-31 14:28:07 +0800

    ci

--- XiaoMa/Class/HKInsurance.h
+++ XiaoMa/Class/HKInsurance.h
@@ -8,9 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-@interface Insurance : SubInsurance
 
-@end
 
 /// 险种
 @interface SubInsurance : NSObject
@@ -31,6 +29,8 @@
 @end
 
 
+
+
 @interface HKInsurance : NSObject
 
 @property (nonatomic,copy)NSString * insuranceName;
@@ -41,5 +41,47 @@
 @property (nonatomic)NSArray * subInsuranceArray;
 
 + (instancetype)insuranceWithJSONResponse:(NSDictionary *)rsp;
+
+@end
+
+
+
+
+
+typedef enum : NSUInteger {
+    InsuranceCompulsory = 1101,//交强险
+    InsuranceTravelTax = 1102, //车船税
+    InsuranceCarDamage,//车辆损失险
+    InsuranceThirdPartyLiability,//第三方责任险
+    InsuranceCarSeatInsuranceOfDriver,//车上人员座位险（司机）
+    InsuranceCarSeatInsuranceOfPassenger,//车上人员座位险（乘客）
+    InsuranceWholeCarStolen,//全车盗抢险
+    InsuranceSeparateGlassBreakage,//玻璃单独破碎险
+    InsuranceSpontaneousLossRisk,//自燃损失险
+    InsuranceWaterLoss,//涉水损失险
+    InsuranceExcludingDeductible4CarDamage,//车损险不计免赔
+    InsuranceExcludingDeductible4ThirdPartyLiability,//第三者责任险不计免赔
+    InsuranceExcludingDeductible4CarSeatInsuranceOfDriver,//车上责任险（司机）不计免赔
+    InsuranceExcludingDeductible4CarSeatInsuranceOfPassenger,//车上责任险（乘客）不计免赔
+    InsuranceExcludingDeductible4WholeCarStolen//全车盗抢险不计免赔
+} InsuranceCategory;
+
+typedef enum : NSUInteger {
+    InsuranceTypeCompulsion,//强制险
+    InsuranceTypeBase,//基本险
+    InsuranceTypeAdditional,//附加险
+    InsuranceTypeContractualTerms//特约条款
+} InsuranceType;
+
+@interface Insurance : SubInsurance
+
+@property (nonatomic,copy)NSString * insuranceName;
+
+@property (nonatomic)InsuranceCategory insCategory;
+
+/**
+ *  是否包含不计免赔
+ */
+@property (nonatomic)BOOL isContainExcludingDeductible;
 
 @end
