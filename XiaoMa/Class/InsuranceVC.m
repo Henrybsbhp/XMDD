e9a4b715036d560b8b80475ebd433eb92108ee2c
jjc <jjc@jtang.cn>
2015-08-31 15:02:43 +0800

    新增保险资料上传页面

--- XiaoMa/Class/InsuranceVC.m
+++ XiaoMa/Class/InsuranceVC.m
@@ -17,7 +17,7 @@
 #import "EnquiryInsuranceVC.h"
 #import "WebVC.h"
 #import "ADViewController.h"
-#import "PaymentHelper.h"
+#import "UploadInsuranceInfoVC.h"
 
 @interface InsuranceVC ()
 @property (nonatomic, strong) ADViewController *advc;
@@ -75,15 +75,14 @@
 }
 
 - (void)actionInsuranceDirectSelling {
-    PaymentHelper *helper = [[PaymentHelper alloc] init];
-    [helper resetForUPPayWithTradeNumber:@"201508271702181435538" targetVC:self];
-    [[helper rac_startPay] subscribeNext:^(id x) {
-        
-    } error:^(NSError *error) {
-        
-    } completed:^{
-        
-    }];
+
+    if ([LoginViewModel loginIfNeededForTargetViewController:self]) {
+        UploadInsuranceInfoVC *vc = [UIStoryboard vcWithId:@"UploadInsuranceInfoVC" inStoryboard:@"Insurance"];
+        [self.navigationController pushViewController:vc animated:YES];
+        [vc setGetNextVCBlock:^UIViewController *(BOOL skip) {
+            return nil;
+        }];
+    }
 }
 
 - (void)actionAiCheBao {
