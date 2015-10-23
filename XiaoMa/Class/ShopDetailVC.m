e8aac9aa727a66872e77c174d219675614878e15
fq <fq@jtang.cn>
2015-10-23 14:03:31 +0800

    控制走马灯宽度

--- XiaoMa/Class/ShopDetailVC.m
+++ XiaoMa/Class/ShopDetailVC.m
@@ -24,6 +24,7 @@
 #import "UIView+Layer.h"
 #import "MyCarStore.h"
 #import "CBAutoScrollLabel.h"
+#import "NSString+RectSize.h"
 
 #define kDefaultServieCount     2
 
@@ -32,6 +33,7 @@
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (weak, nonatomic) IBOutlet UIImageView *headImgView;
 @property (weak, nonatomic) IBOutlet CBAutoScrollLabel *roundLb;
+@property (weak, nonatomic) IBOutlet UIView *roundBgView;
 @property (weak, nonatomic) IBOutlet UIImageView *maskView;
 @property (weak, nonatomic) IBOutlet UIView *titleView;
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
@@ -705,12 +707,22 @@
     self.roundLb.textColor = [UIColor colorWithHex:@"#22b7b5" alpha:1.0f];
     self.roundLb.font = [UIFont systemFontOfSize:14];
     self.roundLb.backgroundColor = self.imageCountLabel.backgroundColor;
+    self.roundBgView.backgroundColor = self.imageCountLabel.backgroundColor;
     self.roundLb.labelSpacing = 30;
     self.roundLb.scrollSpeed = 30;
     self.roundLb.fadeLength = 5.f;
     [self.roundLb observeApplicationNotifications];
     
-    self.roundLb.text = @"推荐客";
+    
+    NSString * note = @"推荐客";
+    
+    CGSize size = [note sizeWithFont:[UIFont systemFontOfSize:14] constrainedToSize:CGSizeMake(FLT_MAX,FLT_MAX)];
+    CGFloat width = MIN(size.width, self.view.frame.size.width - 90);
+    [self.roundLb mas_makeConstraints:^(MASConstraintMaker *make) {
+        
+        make.width.mas_equalTo(width);
+    }];
+    self.roundLb.text = note;
 }
 
 -(BOOL)isBetween:(NSString *)openHourStr and:(NSString *)closeHourStr
