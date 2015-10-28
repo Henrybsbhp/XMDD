c4264902e254ea3eab6928f56da707cff1bb5e07
jjc <jjc@jtang.cn>
2015-10-28 18:50:54 +0800

    修改 CKLimitTextField在真机上崩溃的问题

--- XiaoMa/Class/CKLimitTextField.m
+++ XiaoMa/Class/CKLimitTextField.m
@@ -8,9 +8,10 @@
 
 #import "CKLimitTextField.h"
 
-@interface CKLimitTextField ()<UITextFieldDelegate>
-@property (nonatomic, strong) NSString *oldText;
-@property (nonatomic, strong, readonly) UITextPosition *oldCursorPosition;
+@interface CKLimitTextField ()
+
+@property (nonatomic, strong) CKLimitTextFieldProxyObject *proxyObject;
+
 @end
 
 @implementation CKLimitTextField
@@ -33,66 +34,84 @@
 
 - (void)commonInit
 {
-    self.delegate = self;
-    [self addTarget:self action:@selector(actionTextDidChanged:) forControlEvents:UIControlEventEditingChanged];
+    _proxyObject = [[CKLimitTextFieldProxyObject alloc] init];
+    _proxyObject.textField = self;
+    self.delegate = _proxyObject;
+    [self addTarget:_proxyObject action:@selector(actionTextDidChanged:) forControlEvents:UIControlEventEditingChanged];
     NSLog(@"self.inputdelegate = %@", self.inputDelegate);
 }
 
+
+@end
+
+@interface CKLimitTextFieldProxyObject ()
+@property (nonatomic, strong) NSString *oldText;
+@property (nonatomic, strong, readonly) UITextPosition *oldCursorPosition;
+@end
+@implementation CKLimitTextFieldProxyObject
+
 - (void)actionTextDidChanged:(CKLimitTextField *)textField
 {
-    self.curCursorPosition = textField.selectedTextRange.start;
+    _textField.curCursorPosition = textField.selectedTextRange.start;
     UITextRange *markedRange = textField.markedTextRange;
-    BOOL isAtEnd = [textField comparePosition:self.curCursorPosition toPosition:textField.endOfDocument] == NSOrderedSame;
+    BOOL isAtEnd = [textField comparePosition:_textField.curCursorPosition toPosition:textField.endOfDocument] == NSOrderedSame;
     
     if (!markedRange) {
-        if (self.regexpPattern) {
-            NSRegularExpression *regexp = [NSRegularExpression regularExpressionWithPattern:self.regexpPattern options:0 error:nil];
+        if (_textField.regexpPattern) {
+            NSRegularExpression *regexp = [NSRegularExpression regularExpressionWithPattern:_textField.regexpPattern
+                                                                                    options:0 error:nil];
             NSTextCheckingResult *rst = [regexp firstMatchInString:textField.text options:0 range:NSMakeRange(0, textField.text.length)];
             if (rst.range.location == NSNotFound) {
                 textField.text = self.oldText;
-                self.curCursorPosition = self.oldCursorPosition;
+                _textField.curCursorPosition = self.oldCursorPosition;
             }
             else {
                 textField.text = [textField.text substringWithRange:rst.range];
-                self.curCursorPosition = textField.endOfDocument;
+                _textField.curCursorPosition = textField.endOfDocument;
             }
         }
-        if (self.textChangingBlock) {
-            self.textChangingBlock(textField);
+        if (_textField.textChangingBlock) {
+            _textField.textChangingBlock(textField);
         }
     }
-    if (self.textLimit > 0 && self.textLimit < textField.text.length) {
-        textField.text = [textField.text substringToIndex:self.textLimit];
-        NSInteger diffOffset = [textField offsetFromPosition:self.curCursorPosition toPosition:textField.endOfDocument];
+    if (_textField.textLimit > 0 && _textField.textLimit < textField.text.length) {
+        textField.text = [textField.text substringToIndex:_textField.textLimit];
+        NSInteger diffOffset = [textField offsetFromPosition:_textField.curCursorPosition toPosition:textField.endOfDocument];
         if (!isAtEnd && diffOffset > 0) {
-            textField.selectedTextRange = [textField textRangeFromPosition:self.curCursorPosition toPosition:self.curCursorPosition];
+            textField.selectedTextRange = [textField textRangeFromPosition:_textField.curCursorPosition toPosition:_textField.curCursorPosition];
         }
-        [textField sendActionsForControlEvents:UIControlEventEditingChanged];
+        
+        [self performSelector:@selector(_callEditingChangedActions:) withObject:textField];
     }
     else {
-        NSInteger diffOffset = [textField offsetFromPosition:self.curCursorPosition toPosition:textField.endOfDocument];
+        NSInteger diffOffset = [textField offsetFromPosition:_textField.curCursorPosition toPosition:textField.endOfDocument];
         if (!isAtEnd && diffOffset > 0) {
-            textField.selectedTextRange = [textField textRangeFromPosition:self.curCursorPosition toPosition:self.curCursorPosition];
+            textField.selectedTextRange = [textField textRangeFromPosition:_textField.curCursorPosition toPosition:_textField.curCursorPosition];
         }
-        if (self.textDidChangedBlock) {
-            self.textDidChangedBlock(textField);
+        if (_textField.textDidChangedBlock) {
+            _textField.textDidChangedBlock(textField);
         }
     }
+}
+
+- (void)_callEditingChangedActions:(CKLimitTextField *)textfield
+{
+    [textfield sendActionsForControlEvents:UIControlEventEditingChanged];
 }
 
 #pragma mark - UITextFieldDelegate
 - (void)textFieldDidBeginEditing:(UITextField *)textField
 {
-    if (self.didBeginEditingBlock) {
-        self.didBeginEditingBlock(self);
+    if (_textField.didBeginEditingBlock) {
+        _textField.didBeginEditingBlock(_textField);
     }
 }
 
 - (void)textFieldDidEndEditing:(UITextField *)textField
 {
     _oldCursorPosition = nil;
-    if (self.didEndEditingBlock) {
-        self.didEndEditingBlock(self);
+    if (_textField.didEndEditingBlock) {
+        _textField.didEndEditingBlock(_textField);
     }
 }
 
