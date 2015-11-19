dcbf25260a2098a0371d806833da25c4464b835a
fq <fq@jtang.cn>
2015-11-19 15:43:57 +0800

    网页背景替换

--- XiaoMa/Class/DetailWebVC.m
+++ XiaoMa/Class/DetailWebVC.m
@@ -57,6 +57,8 @@ typedef NS_ENUM(NSInteger, MenuItemsType) {
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.view.backgroundColor = [UIColor colorWithHex:@"#f4f4f4" alpha:1.0f];
+    self.webView.backgroundColor = [UIColor colorWithHex:@"#f4f4f4" alpha:1.0f];
     self.navModel.curNavCtrl = self.navigationController;
     
     [self setupProcessView];
