2742ff5f351ff77701b51aa21768e599b66ef99a
jjc <jjc@jtang.cn>
2015-12-30 14:58:17 +0800

    修改加油支付里的服务协议的文案

--- XiaoMa/Class/PayForInsuranceVC.m
+++ XiaoMa/Class/PayForInsuranceVC.m
@@ -117,7 +117,7 @@
     self.licenseData.customInfo[@"url1"] = [NSURL URLWithString:kInsuranceLicenseUrl];
     if (self.insOrder.licenseUrl.length > 0) {
         NSString *license2 = self.insOrder.licenseName;
-        [license appendFormat:@"及%@", license2];
+        [license appendFormat:@"和%@", license2];
         self.licenseData.customInfo[@"range2"] = [NSValue valueWithRange:NSMakeRange(license.length-license2.length, license2.length)];
         self.licenseData.customInfo[@"url2"] = [NSURL URLWithString:self.insOrder.licenseUrl];
     }
