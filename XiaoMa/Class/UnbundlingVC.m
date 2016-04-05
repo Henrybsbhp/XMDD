bca9034a5ff69956b08ee590d15f3dbddcc31c2f
St. Jimmy <Henrybsbhp@gmail.com>
2016-04-05 21:57:35 +0800

    登陆和银行卡页面。

--- XiaoMa/Class/UnbundlingVC.m
+++ XiaoMa/Class/UnbundlingVC.m
@@ -15,6 +15,7 @@
 #import "UnbindBankcardOp.h"
 #import "UIView+Shake.h"
 #import "BankCardStore.h"
+#import "HKImageAlertVC.h"
 
 static NSString *s_sendedPhone;
 
@@ -26,6 +27,9 @@ static NSString *s_sendedPhone;
 @property (nonatomic, strong) HKSMSModel *smsModel;
 @property (nonatomic, strong) NSAttributedString *promptString;
 @property (nonatomic, strong) UIView *headerView;
+
+@property (nonatomic, strong) HKImageAlertVC *alert;
+
 @end
 
 @implementation UnbundlingVC
@@ -40,6 +44,11 @@ static NSString *s_sendedPhone;
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.smsModel = [[HKSMSModel alloc] init];
+    
+    if (IOSVersionGreaterThanOrEqualTo(@"8.0")) {
+        self.tableView.estimatedRowHeight = 26;
+        self.tableView.rowHeight = UITableViewAutomaticDimension;
+    }
 }
 
 
@@ -77,73 +86,142 @@ static NSString *s_sendedPhone;
         
         @strongify(self);
         [gToast dismiss];
-        [ResultVC showInTargetVC:self withSuccessText:@"解绑成功!" ensureBlock:^{
+        
+        HKAlertActionItem *confirm = [HKAlertActionItem itemWithTitle:@"确认" color:HEXCOLOR(@"#18d06a") clickBlock:^(id alertVC) {
+            [alertVC dismiss];
             [MobClick event:@"rp329_4"];
             [self.navigationController popToViewController:self.originVC animated:YES];
             [self postCustomNotificationName:kNotifyRefreshMyBankcardList object:nil];
         }];
+        HKAlertVC *alert = [self alertWithTopTitle:@"解绑成功" ImageName:@"mins_ok" Message:@"您将无法使用该卡支付了！" ActionItems:@[confirm]];
+        
+        [alert show];
+        
+//        [ResultVC showInTargetVC:self withSuccessText:@"解绑成功!" ensureBlock:^{
+//            [MobClick event:@"rp329_4"];
+//            [self.navigationController popToViewController:self.originVC animated:YES];
+//            [self postCustomNotificationName:kNotifyRefreshMyBankcardList object:nil];
+//        }];
     } error:^(NSError *error) {
         
         [gToast showError:error.domain];
     }];
   
 }
+
+-(HKImageAlertVC *)alertWithTopTitle:(NSString *)topTitle ImageName:(NSString *)imageName Message:(NSString *)message ActionItems:(NSArray *)actionItems
+{
+    if (!_alert)
+    {
+        _alert = [[HKImageAlertVC alloc]init];
+    }
+    _alert.topTitle = topTitle;
+    _alert.imageName = imageName;
+    _alert.message = message;
+    _alert.actionItems = actionItems;
+    return _alert;
+}
+
 #pragma mark - TableView
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    return 50;
+    if (IOSVersionGreaterThanOrEqualTo(@"8.0")) {
+        
+        return UITableViewAutomaticDimension;
+        
+    }
+    
+    UITableViewCell *cell = [self tableView:self.tableView cellForRowAtIndexPath:indexPath];
+    [cell layoutIfNeeded];
+    [cell setNeedsUpdateConstraints];
+    [cell updateConstraintsIfNeeded];
+    CGSize size = [cell.contentView systemLayoutSizeFittingSize:UILayoutFittingExpandedSize];
+    
+    return ceil(size.height + 1);
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 1;
+    return 2;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
 {
-    return 40;
+    return CGFLOAT_MIN;
 }
 
-- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
-{
-    if (!self.headerView) {
-        UIView *headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 40)];
-        UILabel * lb = [[UILabel alloc] initWithFrame:CGRectZero];
-        lb.numberOfLines = 2;
-        lb.lineBreakMode = NSLineBreakByWordWrapping;
-        lb.font = [UIFont systemFontOfSize:14];
-        lb.textColor = [UIColor darkGrayColor];
-        lb.backgroundColor = [UIColor clearColor];
-        lb.tag = 1001;
-        headerView.backgroundColor = [UIColor clearColor];
-        [headerView addSubview:lb];
-        [lb mas_makeConstraints:^(MASConstraintMaker *make) {
-            
-            make.left.equalTo(headerView.mas_left).offset(16);
-            make.centerY.equalTo(headerView.mas_centerY).offset(6);
-            make.size.height.mas_equalTo(36);
-            make.right.equalTo(headerView.mas_right).offset(-16);
-        }];
-        self.headerView = headerView;
-    }
-
-    UILabel *lb = (UILabel *)[self.headerView viewWithTag:1001];
-    @weakify(self);
-    [[RACObserve(self, promptString) takeUntil:[self.headerView rac_signalForSelector:@selector(prepareForReuse)]]
-     subscribeNext:^(id x) {
-        @strongify(self);
-        lb.attributedText = self.promptString;
-    }];
-
-
-    return self.headerView;
-}
+//- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+//{
+//    if (!self.headerView) {
+//        UIView *headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 40)];
+//        UILabel * lb = [[UILabel alloc] initWithFrame:CGRectZero];
+//        lb.numberOfLines = 2;
+//        lb.lineBreakMode = NSLineBreakByWordWrapping;
+//        lb.font = [UIFont systemFontOfSize:14];
+//        lb.textColor = [UIColor darkGrayColor];
+//        lb.backgroundColor = [UIColor clearColor];
+//        lb.tag = 1001;
+//        headerView.backgroundColor = [UIColor clearColor];
+//        [headerView addSubview:lb];
+//        [lb mas_makeConstraints:^(MASConstraintMaker *make) {
+//            
+//            make.left.equalTo(headerView.mas_left).offset(16);
+//            make.centerY.equalTo(headerView.mas_centerY).offset(6);
+//            make.size.height.mas_equalTo(36);
+//            make.right.equalTo(headerView.mas_right).offset(-16);
+//        }];
+//        self.headerView = headerView;
+//    }
+//
+//    UILabel *lb = (UILabel *)[self.headerView viewWithTag:1001];
+//    @weakify(self);
+//    [[RACObserve(self, promptString) takeUntil:[self.headerView rac_signalForSelector:@selector(prepareForReuse)]]
+//     subscribeNext:^(id x) {
+//        @strongify(self);
+//        lb.attributedText = self.promptString;
+//    }];
+//
+//
+//    return self.headerView;
+//}
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"normalCell" forIndexPath:indexPath];
+    UITableViewCell *cell;
     
+    if (indexPath.section == 0 && indexPath.row == 0) {
+        cell = [self loadNormalCellAtIndexPath:indexPath];
+        
+    } else if (indexPath.row == 1) {
+        
+        cell = [tableView dequeueReusableCellWithIdentifier:@"ConfirmButtonCell"];
+        
+    }
+    
+    return cell;
+}
+
+- (UITableViewCell *)loadNormalCellAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"normalCell"];
     UITextField *textfield = (UITextField *)[cell.contentView viewWithTag:1001];
     UIButton *vcodeBtn = (UIButton *)[cell.contentView viewWithTag:1002];
+    UILabel *tipsLabel = (UILabel *)[cell.contentView viewWithTag:1003];
+    tipsLabel.preferredMaxLayoutWidth = gAppMgr.deviceInfo.screenSize.width - 34;
+    
+    textfield.layer.borderColor = [[UIColor colorWithHTMLExpression:@"#EEEFEF"] CGColor];
+    textfield.layer.cornerRadius = 1;
+    textfield.layer.borderWidth = 1;
+    textfield.layer.masksToBounds = YES;
+    
+    tipsLabel.attributedText = [self attrStrWithPhone:gAppMgr.myUser.userID];
+    UIColor *placeholderColor = [UIColor colorWithHTMLExpression:@"#DBDBDB"];
+    NSAttributedString *string = [[NSAttributedString alloc] initWithString:@"请输入手机验证码" attributes:@{ NSForegroundColorAttributeName : placeholderColor }];
+    textfield.attributedPlaceholder = string;
     
     [[[textfield rac_signalForControlEvents:UIControlEventEditingDidBegin] takeUntil:[cell rac_prepareForReuseSignal]] subscribeNext:^(id x) {
         [MobClick event:@"rp329_1"];
@@ -173,7 +251,7 @@ static NSString *s_sendedPhone;
 {
     
 }
-     
+
 #pragma mark - Utility
 - (NSAttributedString *)attrStrWithError:(NSError *)error
 {
@@ -182,10 +260,14 @@ static NSString *s_sendedPhone;
      
 - (NSAttributedString *)attrStrWithPhone:(NSString *)phone
 {
+    NSString *last4CardNumber = [self.card.cardNumber substringFromIndex:MAX((int)[self.card.cardNumber length]-4, 0)];
+    
     phone = [HKConvertModel convertPhoneNumberForEncryption:phone];
-    NSMutableAttributedString *attrStr = [[NSMutableAttributedString alloc] initWithString:@"已发送验证码至" attributes:nil];
-    [attrStr appendAttributedString:[[NSAttributedString alloc] initWithString:phone attributes:@{NSForegroundColorAttributeName:HEXCOLOR(@"#23ac2d")}]];
-    [attrStr appendAttributedString:[[NSAttributedString alloc] initWithString:@"，请输入验证码解绑~" attributes:nil]];
+    NSMutableAttributedString *attrStr = [[NSMutableAttributedString alloc] initWithString:@"解绑尾号为" attributes:nil];
+    [attrStr appendAttributedString:[[NSAttributedString alloc] initWithString:last4CardNumber]];
+    [attrStr appendAttributedString:[[NSAttributedString alloc] initWithString:@"的银行卡，验证码已发送至"]];
+    [attrStr appendAttributedString:[[NSAttributedString alloc] initWithString:phone attributes:@{NSForegroundColorAttributeName:HEXCOLOR(@"#18D06A")}]];
+    [attrStr appendAttributedString:[[NSAttributedString alloc] initWithString:@"，" attributes:nil]];
     return attrStr;
 }
 
