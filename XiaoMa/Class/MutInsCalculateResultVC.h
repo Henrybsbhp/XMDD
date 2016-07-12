e521e1b5973f25f34dd08acdadee59ea5fc9cf31
RockyYe <soft@RockyYedeMacBook-Pro.local>
2016-07-12 13:58:39 +0800

    no message

---
+++ XiaoMa/Class/MutInsCalculateResultVC.h
@@ -0,0 +1,13 @@
+//
+//  MutInsCalculateResultVC.h
+//  XiaoMa
+//
+//  Created by RockyYe on 16/7/12.
+//  Copyright © 2016年 huika. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MutInsCalculateResultVC : UIViewController
+
+@end
---
+++ XiaoMa/Class/MutInsCalculateResultVC.m
@@ -0,0 +1,37 @@
+//
+//  MutInsCalculateResultVC.m
+//  XiaoMa
+//
+//  Created by RockyYe on 16/7/12.
+//  Copyright © 2016年 huika. All rights reserved.
+//
+
+#import "MutInsCalculateResultVC.h"
+
+@interface MutInsCalculateResultVC ()
+
+@end
+
+@implementation MutInsCalculateResultVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
