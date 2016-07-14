5d0c178f19888f1340c6c87ace48a3a27f16e278
RockyYe <soft@RockyYedeMacBook-Pro.local>
2016-07-14 17:11:38 +0800

    no message

---
+++ XiaoMa/Class/MutualInsAdModel.h
@@ -0,0 +1,13 @@
+//
+//  MutualInsAdModel.h
+//  XiaoMa
+//
+//  Created by RockyYe on 16/7/14.
+//  Copyright © 2016年 huika. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MutualInsAdModel : NSObject
+
+@end
---
+++ XiaoMa/Class/MutualInsAdModel.m
@@ -0,0 +1,13 @@
+//
+//  MutualInsAdModel.m
+//  XiaoMa
+//
+//  Created by RockyYe on 16/7/14.
+//  Copyright © 2016年 huika. All rights reserved.
+//
+
+#import "MutualInsAdModel.h"
+
+@implementation MutualInsAdModel
+
+@end
