c617328a2068893abea4280c814a4162d4badc7e
St. Jimmy <Henrybsbhp@gmail.com>
2016-07-19 15:27:57 +0800

    Apple Pay Helper 集成。

---
+++ XiaoMa/Class/UPApplePayHelper.h
@@ -0,0 +1,13 @@
+//
+//  UPApplePayHelper.h
+//  XiaoMa
+//
+//  Created by St.Jimmy on 7/19/16.
+//  Copyright © 2016 huika. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UPApplePayHelper : NSObject
+
+@end
---
+++ XiaoMa/Class/UPApplePayHelper.m
@@ -0,0 +1,13 @@
+//
+//  UPApplePayHelper.m
+//  XiaoMa
+//
+//  Created by St.Jimmy on 7/19/16.
+//  Copyright © 2016 huika. All rights reserved.
+//
+
+#import "UPApplePayHelper.h"
+
+@implementation UPApplePayHelper
+
+@end
