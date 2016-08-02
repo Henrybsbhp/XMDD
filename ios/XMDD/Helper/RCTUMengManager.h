1be281731e710ad9f6993f8e5fa1387966df3668
jiangjunchen <jiangjunchen@xiaomadada.com>
2016-08-02 17:04:38 +0800

    reslove warnning

---
+++ ios/XMDD/Helper/RCTUMengManager.h
@@ -0,0 +1,14 @@
+//
+//  RCTUMengManager.h
+//  XMDD
+//
+//  Created by jiangjunchen on 16/8/2.
+//  Copyright © 2016年 huika. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <RCTBridgeModule.h>
+
+@interface RCTUMengManager : NSObject <RCTBridgeModule>
+
+@end
