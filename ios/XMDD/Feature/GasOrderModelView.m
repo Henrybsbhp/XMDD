2073929d1f2ffb7569aaffc629ee83a6cd91aefb
St. Jimmy <Henrybsbhp@gmail.com>
2016-08-31 09:47:26 +0800

    我的订单页面 cardType 类型调整。1：中石化，2：中石油

--- ios/XMDD/Feature/GasOrderModelView.m
+++ ios/XMDD/Feature/GasOrderModelView.m
@@ -119,12 +119,12 @@
             
             NSDate *date = [NSDate dateWithUTS:@(order.payedTime)];
             if (order.cardType == 1) {
-                brandImageView.image = [UIImage imageNamed:@"gas_icon_cnpc"];
-            } else {
                 brandImageView.image = [UIImage imageNamed:@"gas_icon_snpn"];
+            } else {
+                brandImageView.image = [UIImage imageNamed:@"gas_icon_cnpc"];
             }
             
-            titleLabel.text = order.cardType == 1 ? @"中石油" : @"中石化";
+            titleLabel.text = order.cardType == 1 ? @"中石化" : @"中石油";
             statusLabel.text = order.statusDesc;
             cardNumLabel.text = [order.gasCardNum splitByStep:4 replacement:@" "];
             originalPriceLabel.text = [NSString stringWithFormat:@"¥%@", order.chargeMoney];
