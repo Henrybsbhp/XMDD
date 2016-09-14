24f7291a78cef7db7f95f44e8a0994fb4e3aaf3c
jiangjunchen <jiangjunchen@xiaomadada.com>
2016-09-14 11:32:40 +0800

    tempci

--- src/component/mutual_ins/GroupDetailFundView.js
+++ src/component/mutual_ins/GroupDetailFundView.js
@@ -3,7 +3,9 @@ import React, {Component} from 'react';
 import {View, StyleSheet, ART} from 'react-native';
 import SegmentView from '../general/SegmentView';
 import UI from '../../constant/UIConstants';
+import Art from '../general/shaps/Arc';
 
+const CircleLength = 110;
 
 export default class GroupDetailFundView extends Component {
     constructor(props) {
@@ -13,12 +15,40 @@ export default class GroupDetailFundView extends Component {
     render() {
         return (
             <View style={styles.container}>
-
+                {this.renderProgress()}
             </View>
+        )
+    }
+
+    renderProgress() {
+        return (
+            <View style={styles.progressContainer}>
+            <ART.Surface width={2*CircleLength} height={2*CircleLength}>
+                <Art
+                    radius={CircleLength - 0.5}
+                    stroke={UI.Color.Background}
+                    strokeWidth={3}
+                    offset={{top: 0.5, left: 0.5}}
+                    startAngle={0}
+                    endAngle={2 * Math.PI}
+                />
+                <Art
+                    radius={CircleLength}
+                    stroke={UI.Color.DefaultTint}
+                    strokeWidth={4}
+                    offset={{top: 0, left: 0}}
+                    startAngle={0}
+                    endAngle={Math.PI}
+                />
+            </ART.Surface>
+                </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
-    container: {backgeoundColor: 'white', flex: 1},
+    container: {backgroundColor: 'white', marginTop: 8},
+    progressContainer: {
+        width: 2 * CircleLength, height: 2 * CircleLength, alignSelf: 'center', marginTop: 20, marginBottom: 15
+    },
 })
