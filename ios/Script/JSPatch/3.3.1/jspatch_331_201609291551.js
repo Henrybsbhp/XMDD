2501dd0ee653218f16e67822b850042550845081
fq <fuqi@xiaomadada.com>
2016-09-29 16:07:05 +0800

    修复小手机的违章问题

--- ios/Script/JSPatch/3.3.1/jspatch_331_201609291551.js
+++ ios/Script/JSPatch/3.3.1/jspatch_331_201609291551.js
@@ -1,7 +1,4 @@
 require('JPEngine').addExtensions(['JPMacroSupport'])
-require('MyCarStore')
-require('ADViewController')
-require('MASConstraintMaker')
 require('CKDict')
 
 defineClass('MyCarStore',{
@@ -11,48 +8,6 @@ defineClass('MyCarStore',{
   	return self.defalutCar();
   }
 });
-
-defineClass('GasVC',{
-
-  setupADView:function () {
-    
-  	if (!self.adctrl())
-  	{
-  		var ww =  self.view().bounds().width;
-  		self.setAdctrl(require('ADViewController').vcWithADType_boundsWidth_targetVC_mobBaseEvent_mobBaseEventDict(4,ww,self,"rp501_1",null));
-  	}
-
-  	
-  	var weakSelf = __weak(self)
-  	self.adctrl().reloadDataWithForce_completed(false,block("ADViewController *,NSArray *",function(ctrl,ads){
-
-		var strongSelf = __strong(weakSelf);
-		var header = strongSelf.headerView();
-
-  		if (ads.count() == 0 || header.subviews().containsObject(ctrl.adView()))
-  		{
-			return;  			
-  		}
-
-  		var height = ctrl.adView().frame().height;
-  		var originHeight = header.frame().height;
-  		header.setFrame({x:0, y:0, width:strongSelf.view().frame().height, height:height+originHeight});
-  		header.addSubview(ctrl.adView());
-  		ctrl.adView().mas__makeConstraints(block("MASConstraintMaker *",function (make) {
-  			
-  			if (header)
-  			{	
-  				make.left().equalTo()(header);
-  				make.right().equalTo()(header);
-  				make.top().equalTo()(header);
-  				make.height().equalTo()((height));
-  			}
-  		}))
-  		strongSelf.tableView().setTableHeaderView(header);
-  	}))
-  }
-});
-
 
 defineClass('ViolationItemViewController',{
 
@@ -116,7 +71,14 @@ defineClass('ViolationItemViewController',{
     item.setObject_forKeyedSubscript(block("CKDict *,UITableViewCell *,NSIndexPath *", prepareBlock),"__preparecell");
     item.setObject_forKeyedSubscript(block("CKDict *,NSIndexPath *", heightBlock),"__getcellheight");
 
-    return item;
+    if (require("AppManager").sharedManager().deviceInfo().screenSize().height > 667)
+    {
+      return item;
+    }
+    else
+    {
+      return self.ORIGsetupViolationItemCell(violation);
+    }
   }
 
 
--- ios/Script/JSPatch/3.4.0/jspatch_340_201609291551.js
+++ ios/Script/JSPatch/3.4.0/jspatch_340_201609291551.js
@@ -1,4 +1,7 @@
 require('JPEngine').addExtensions(['JPMacroSupport'])
+require('MyCarStore')
+require('ADViewController')
+require('MASConstraintMaker')
 require('CKDict')
 
 defineClass('MyCarStore',{
@@ -8,6 +11,48 @@ defineClass('MyCarStore',{
   	return self.defalutCar();
   }
 });
+
+defineClass('GasVC',{
+
+  setupADView:function () {
+    
+  	if (!self.adctrl())
+  	{
+  		var ww =  self.view().bounds().width;
+  		self.setAdctrl(require('ADViewController').vcWithADType_boundsWidth_targetVC_mobBaseEvent_mobBaseEventDict(4,ww,self,"rp501_1",null));
+  	}
+
+  	
+  	var weakSelf = __weak(self)
+  	self.adctrl().reloadDataWithForce_completed(false,block("ADViewController *,NSArray *",function(ctrl,ads){
+
+		var strongSelf = __strong(weakSelf);
+		var header = strongSelf.headerView();
+
+  		if (ads.count() == 0 || header.subviews().containsObject(ctrl.adView()))
+  		{
+			return;  			
+  		}
+
+  		var height = ctrl.adView().frame().height;
+  		var originHeight = header.frame().height;
+  		header.setFrame({x:0, y:0, width:strongSelf.view().frame().height, height:height+originHeight});
+  		header.addSubview(ctrl.adView());
+  		ctrl.adView().mas__makeConstraints(block("MASConstraintMaker *",function (make) {
+  			
+  			if (header)
+  			{	
+  				make.left().equalTo()(header);
+  				make.right().equalTo()(header);
+  				make.top().equalTo()(header);
+  				make.height().equalTo()((height));
+  			}
+  		}))
+  		strongSelf.tableView().setTableHeaderView(header);
+  	}))
+  }
+});
+
 
 defineClass('ViolationItemViewController',{
 
@@ -65,14 +110,20 @@ defineClass('ViolationItemViewController',{
 
       var height = 63 + size1.height + 16 + size2.height + 14;
       var height2 =  Math.ceil(height)
-      return height2;
+      console.log("heightBlock"+height2);
+      return 100;
     }
 
     item.setObject_forKeyedSubscript(block("CKDict *,UITableViewCell *,NSIndexPath *", prepareBlock),"__preparecell");
     item.setObject_forKeyedSubscript(block("CKDict *,NSIndexPath *", heightBlock),"__getcellheight");
 
-    return item;
+    if (require("AppManager").sharedManager().deviceInfo().screenSize().height > 667)
+    {
+      return item;
+    }
+    else
+    {
+      return self.ORIGsetupViolationItemCell(violation);
+    }
   }
-
-
 });
