a4e6b41ff53ac696ddbd0981433e5a440daec75e
St. Jimmy <Henrybsbhp@gmail.com>
2016-10-31 13:07:57 +0800

    「救援，协办」页面文案修改。

--- ios/XMDD/Feature/RescuePaymentStatusVC.m
+++ ios/XMDD/Feature/RescuePaymentStatusVC.m
@@ -126,7 +126,7 @@
         @strongify(self);
         [self.view stopActivityAnimation];
         self.bottomView.hidden = YES;
-        [self.view showImageEmptyViewWithImageName:@"def_withoutAssistHistory" text:@"暂无救援记录" tapBlock:^{
+        [self.view showImageEmptyViewWithImageName:@"def_failConnect" text:kDefErrorPormpt tapBlock:^{
             @strongify(self);
             [self requestForRescueDetailData];
         }];
